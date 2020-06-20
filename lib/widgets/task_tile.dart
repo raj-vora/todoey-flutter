11b113c16436f59bc1a0398f6d273c04aa9d6525
raj-vora <raj.jv@somaiya.edu>
2020-06-20 13:22:40 +0530

    made checkbox responsive, state passed up to tasktile

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,11 +1,46 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('This is a task'),
-      trailing: Checkbox(value: false, onChanged: null),
+      title: Text(
+        'This is a task',
+        style: TextStyle(
+          decoration: isChecked ? TextDecoration.lineThrough : null
+        ),
+      ),
+      trailing: TaskCheckbox(
+        checkBoxState: isChecked,
+        toggleCheckBoxState: (bool checkBoxState) {
+          setState(() {
+            isChecked = checkBoxState;
+          });
+        },
+      ),
+    );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+  final bool checkBoxState;
+  final Function toggleCheckBoxState;
+
+  TaskCheckbox({this.checkBoxState, this.toggleCheckBoxState});
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: checkBoxState, 
+      onChanged: toggleCheckBoxState,
+      activeColor: Colors.lightBlueAccent,
     );
   }
 }
